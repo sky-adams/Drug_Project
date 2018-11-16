a93b15a896469a87cd89fe7ef44a2d50823b7a81
Mesa Diehl <42652916+DaZenMesa@users.noreply.github.com>
2018-11-16 10:21:15 -0800

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+  def render_main():
+  return render_template("mainpage.html")
+
+if __name__=="__main__":
+    app.run(debug=True, port=54321)
